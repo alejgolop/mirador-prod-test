a9c8b44c722ff186c17e94a1efe3c72dfa5da2e8
Alejandro Gómez <alejgolop@gmail.com>
2023-07-21 12:55:19 +0200

    s

--- js/ra-mirador.js
+++ js/ra-mirador.js
@@ -1,7 +1,5 @@
-const params = new Proxy(new URLSearchParams(window.location.search), {
-  get: (searchParams, prop) => searchParams.get(prop),
-});
-const mockPosition=params.mockoffice!==undefined;
+
+const mockPosition=window.location.search.includes('mockoffice');
 
 var data;
 var swiper = undefined;
